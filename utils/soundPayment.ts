e33e4c4b13a0e690e8b6236f8f6de93f46214b67
Mark Flynn <markflynn@Marks-MacBook-Pro.local>
2026-01-23 21:02:18 +0000

    fix: increase debounce to 45ms to prevent double detection

--- utils/soundPayment.ts
+++ utils/soundPayment.ts
@@ -392,7 +392,7 @@ export async function startListening(
           if (charResult && charResult.mode === currentMode) {
             const { char } = charResult;
             
-            if (char !== lastChar && now - lastCharTime > 35) {
+            if (char !== lastChar && now - lastCharTime > 45) {
               receivedChars.push(char);
               lastChar = char;
               lastCharTime = now;
